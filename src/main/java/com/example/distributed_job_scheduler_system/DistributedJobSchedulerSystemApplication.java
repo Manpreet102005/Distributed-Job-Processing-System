ac0673336945f51f427ec5b032a87e95fb7ba2ff
Priyanshi185 <priyanshichoudhary0912@gmail.com>
2026-07-18 23:05:50 +0530

    creating entity

---
+++ src/main/java/com/example/distributed_job_scheduler_system/DistributedJobSchedulerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributed_job_scheduler_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedJobSchedulerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedJobSchedulerSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/distributed_job_scheduler_system/DistributedJobSchedulerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributed_job_scheduler_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedJobSchedulerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
